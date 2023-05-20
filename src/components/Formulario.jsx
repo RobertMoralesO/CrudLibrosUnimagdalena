b6ccdb5290b511a54b667f3262920f09ac8449fc
Roberto Morales <roberto@morales.com.co>
2023-05-20 09:34:52 -0500

    interfaz ininicial terminada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
 const Formulario = () => {
+    const [nombreLibro, setNombreLibro] = useState('')
+    const [nombreAutor, setNombreAutor] = useState('')
+
   return (
-    <div>Formulario</div>
+    <div className='container mt-5'>
+        <h1 className='text-center'>CRUD DE LIBROS</h1>
+        <hr/>
+        <div className="row">
+            <div className="col-8">
+                <h4 className="text-center">Listado de Libros</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <span className="lead">Test</span>
+                        <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
+                        <button className="btn btn-warning btn-sm float-end">Editar</button>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-4">
+                <h4 className="text-center">AGREGAR LIBROS</h4>
+                <form>
+                    <input type="text" className="form-control mb-2" placeholder='Ingrese Nombre del Libro' />
+                    <input type="text" className="form-control mb-2" placeholder='Ingrese Autor del Libro' />
+                    <button className="btn btn-primary btn-block">Agregar</button>
+                </form>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
+
 export default Formulario
+
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFireStore} from 'firebase/firestore';
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCy9UY-g02SKuXffGsLpKExOhBuuG5M_iI",
@@ -13,5 +13,5 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFireStore(app)
+const db = getFirestore(app)
 export {db}
